7a6e3870d46d622715c46dd5739631a819f06ff3
Aiyern30 <ianbian2@gmail.com>
2025-02-05 13:13:57 +0800

    added tng into payment details

--- src/components/pages/PaymentDetails.tsx
+++ src/components/pages/PaymentDetails.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import { Card, CardContent, CardHeader, CardTitle } from '../ui';
-import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui';
+import Image from 'next/image';
+import { Card } from '../ui';
 
 const paymentMethods = [
   {
     name: 'PayPal',
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/b/b5/PayPal.svg',
-    link: 'https://www.paypal.com/paypalme/aiyern30',
-    description: 'Send tips via PayPal.'
+    logo: '/Logo/paypal.png',
+    link: 'https://www.paypal.com/paypalme/aiyern30'
   },
   {
     name: 'Buy Me a Coffee',
-    logo: 'Logo/buymeacoffee.svg',
-    link: 'https://buymeacoffee.com/IanGan',
-    description: 'Support me with a coffee!'
+    logo: '/Logo/buymeacoffee.svg',
+    link: 'https://buymeacoffee.com/IanGan'
   },
   {
     name: 'Stripe',
-    logo: 'Logo/stripe.png',
-    link: 'https://buy.stripe.com/test_7sIg2FeFCfvu2FW144',
-    description: 'Donate securely via Stripe.'
+    logo: '/Logo/stripe.png',
+    link: 'https://buy.stripe.com/test_7sIg2FeFCfvu2FW144'
+  },
+  {
+    name: "Touch 'n Go eWallet",
+    logo: '/Logo/TNG.jpg', 
+    link: 'https://payment.tngdigital.com.my/sc/bDLnPgpH5S'
   }
 ];
 
@@ -29,20 +31,20 @@ const PaymentDetails = () => {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {paymentMethods.map((method, index) => (
           <a
-          href={method.link}
-          target="_blank"
-          rel="noopener noreferrer"
-          key={index}
-        >
-          <Card key={index} className="bg-white shadow-lg rounded-lg p-4">
-            <CardHeader className="flex items-center space-x-4">
-              <Avatar>
-                <AvatarImage src={method.logo} alt={method.name} />
-                <AvatarFallback>{method.name[0]}</AvatarFallback>
-              </Avatar>
-            </CardHeader>
-            
-          </Card>
+            href={method.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            key={index}
+          >
+            <Card key={index} className="bg-white shadow-lg rounded-lg overflow-hidden h-32 w-32 relative flex items-center justify-center">
+              <Image 
+                src={method.logo} 
+                alt={method.name} 
+                width={64}
+                height={64}
+                priority
+              />
+            </Card>
           </a>
         ))}
       </div>
