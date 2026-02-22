69f98d1ad71430cb672ff61ac18b23fdbcf569d9
Aiyern30 <ianbian2@gmail.com>
2026-02-22 19:51:51 +0800

    `Added support for internationalization to the Not Found page`

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui";
+import { getTranslations } from "next-intl/server";
 
-export default function NotFound() {
+export default async function NotFound() {
+  const t = await getTranslations("notFound");
+
   return (
     <div className="h-screen flex items-center justify-center -p-5">
       <div className="text-center space-y-6 px-4">
-        <h1 className="text-4xl font-medium text-white">Oops!</h1>
+        <h1 className="text-4xl font-medium text-white">{t("oops")}</h1>
         <div className="relative w-[300px] h-[200px] mx-auto">
           <Image
-            src="Logo/404.svg"
+            src="/Logo/404.svg"
             alt="404 Construction Barrier"
             fill
             className="object-contain ml-12"
@@ -18,22 +21,19 @@ export default function NotFound() {
         </div>
         <div className="space-y-4">
           <h2 className="text-2xl font-semibold text-white tracking-wide">
-            PAGE NOT FOUND
+            {t("title")}
           </h2>
-          <p className="text-white max-w-[400px] mx-auto">
-            Sorry, the page you&apos;re looking for doesn&apos;t exist. If you
-            think somethin is broken, report a problem.
-          </p>
+          <p className="text-white max-w-[400px] mx-auto">{t("description")}</p>
         </div>
         <div className="flex items-center justify-center gap-4 pt-2">
           <Button variant="secondary" asChild>
             <Link href="/" className="text-white">
-              GO HOME
+              {t("goHome")}
             </Link>
           </Button>
           <Button variant="secondary" asChild>
             <Link href="/#contact-us" className="text-white">
-              CONTACT US
+              {t("contactUs")}
             </Link>
           </Button>
         </div>
